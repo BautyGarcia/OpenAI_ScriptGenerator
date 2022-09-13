a52459f02f92f7ee0b9118a7352804b3823b3b4a
unknown <ADM@B-19F0371.belgrano.ort.arg>
2022-09-13 10:54:22 -0300

    Now openai

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ IMDB = imdb.IMDb()
 
 openai.api_key = st.text_input("OpenAI API Key")
 
-movieName = st.text_input("Enter a movie name", "Supercool")
+movieName = st.text_input("Enter a movie name")
 
 moviesInfo = IMDB.search_movie(movieName)
 
@@ -16,7 +16,17 @@ movie = IMDB.get_movie(movieID)
 
 plot = ""
 
-for a in range(5):
+for a in range(3):
     plot += movie['plot'][a]
 
-st.write(plot)
+def generate_prompt(Plot):
+return """From a given movie plot, create a script that could fit in the movie between the characters you can identify from the plot: """ + Plot
+
+response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt=generate_prompt(plot),
+    temperature=1,
+    max_tokens = 4000,
+)
+
+st.write(response['choices'][0]['text'])
