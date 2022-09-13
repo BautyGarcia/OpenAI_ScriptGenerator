d72b2032986c63dca8899a0cbfff6d8a5578d0ec
unknown <ADM@B-19F0371.belgrano.ort.arg>
2022-09-13 10:34:28 -0300

    testing imdb

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import openai
+import IMDbPY as imdb
 
-numero = st.number_input("Ingrese un número", 0, 100, 1)
-st.write("El número ingresado es:", numero + 5)
+imdb = imdb.IMDb()
+
+movieName = st.text_input("Enter a movie name")
+
+moviesInfo = imdb.search_movie(movieName)
+
+movieID = moviesInfo[0].movieID
+
+movie = imdb.get_movie(movieID)
+
+movieTitle = movie['title']
+
+st.write(movieTitle)
